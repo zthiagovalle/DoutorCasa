0219549ab59252b3937a1b1dd45963663f4a30f1
Thiago Valle <zthiagovalle@gmail.com>
2021-06-06 16:35:09 -0300

    Atualizado versão

--- main.py
+++ main.py
@@ -1,49 +1,164 @@
 import requests
 import json
-import os
 
 class DoutorCasaBot:
   def __init__(self):
-    token = 'coloque aqui o seu token'
-    self.url_base = f'https://api.telegram.org/bot{token}/'
+      token = 'colo seu token aqui'
+      self.url_base = f'https://api.telegram.org/bot{token}/'
+      self.risco = int(0)
+      self.sequencia = int(0)
 
-  # Iniciar o bot
+      self.msgFebre = '''
+      Você tem febre?
+      1 - Não
+      2 - Sim, abaixo de 38º
+      3 - Sim, 38º ou acima 🤒
+      Digite um dos números acima: 
+      '''
+      self.msgTosse = '''
+      Você tem tosse?
+      1 - Não
+      2 - Tosse seca
+      3 - Sim, tosse com catarro
+      Digite um dos números acima:
+      '''
+      
+      self.msgRespiracao = '''
+      Você está com dificuldades para respirar?
+      1 - Não
+      2 - Sim, mas não me impede de fazer tarefas simples
+      3 - Sim, me impede de fazer tarefas simples 😟
+      Digite um dos números acima:      
+      '''
+
+      self.msgNaoEntendi = 'Não consegui entender, digite iniciar para voltar ao começo do atendimento.'
+
   def Iniciar(self):
-    update_id = None
-    while True:
-      atualizacao = self.obterMensagens(update_id)
-      mensagens = atualizacao['result']
-      if mensagens:
-        for mensagem in mensagens:
-          update_id = mensagem['update_id']
-          chat_id = mensagem['message']['from']['id']
-          primeiraMensagem = mensagem['message']['message_id'] == 1
-          resposta = self.criarResposta(mensagem, primeiraMensagem)
-          self.responderUsuario(resposta,chat_id)
+      update_id = None
+      while True:
+          atualizacao = self.obterNovasMensagens(update_id)
+          mensagens = atualizacao["result"]
+          if mensagens:
+              for mensagem in mensagens:
+                  print(mensagem)
+                  try:
+                    update_id = mensagem['update_id']
+                    usuario = mensagem["message"]["from"]["first_name"]
+                    chat_id = mensagem["message"]["from"]["id"]
+                    primeiraMensagem = mensagem["message"]["message_id"] == 1
+                    msgUsuario = str(mensagem["message"]["text"]).upper()
+                    resposta = self.criarResposta(primeiraMensagem, msgUsuario, usuario)
+                    self.respondeUsuario(resposta, chat_id)
+                  except Exception:
+                    self.respondeUsuario("Tive um problema, me desculpe.\n Envie uma nova mensagem.")
 
-  def obterMensagens(self, update_id):
-    requisicao = f'{self.url_base}getUpdates?timeout=100'
+  def obterNovasMensagens(self, update_id):
+    link_requisicao = f'{self.url_base}getUpdates?timeout=100'
     if update_id:
-      requisicao = f'{requisicao}&offset={update_id + 1}'
-    resultado = requests.get(requisicao)
+        link_requisicao = f'{link_requisicao}&offset={update_id + 1}'
+    resultado = requests.get(link_requisicao)
     return json.loads(resultado.content)
+  
+  def criarResposta(self, primeiraMensagem, msgUsuario, usuario):
+    if(primeiraMensagem == True or msgUsuario in ('OI', 'OLÁ', 'OLA', 'HEY', 'EI',  'BOM DIA', 'BOA TARDE', 'BOA NOITE',  '/START', 'INICIAR')):
+      self.sequencia += 1
+      return"Olá eu sou o seu doutor preferido.\nVou fazer algumas perguntas sobre seu estado de saúde atual.\n\nComo você está se sentindo hoje?\n1 - Ótimo 😀\n2 - Regular\n3 - Mal 🤧\nDigite um dos números acima:"
+      
+    elif self.sequencia == 1:
+      if(msgUsuario == '1' or msgUsuario in('ÓTIMO', 'OTIMO')):
+        self.sequencia += 1
+        return self.msgFebre        
 
-  def criarResposta(self, mensagem, primeiraMensagem):
-    mensagem = mensagem['message']['text']
-    if(primeiraMensagem == True):
-      return f'''Olá eu sou o Doutor Casa{os.linesep}
-        Como você está se sentindo hoje?{os.linesep}
-          1 - Ótimo 😀{os.linesep}
-          2 - Bem{os.linesep}
-          3 - Regular{os.linesep}
-          4 - Mal 🤧{os.linesep}
-        digite um dos números acima: '''
+      elif(msgUsuario == '2' or msgUsuario in('REGULAR')):
+        self.sequencia += 1
+        self.risco += 1
+        return self.msgFebre
+
+      elif(msgUsuario == '3' or msgUsuario in('MAL', 'PESSIMO', 'PÉSSIMO', 'HORRIVEL')):
+        self.sequencia += 1
+        self.risco += 2
+        return self.msgFebre
+
+      else:
+        self.sequencia = 0
+        self.risco = 0
+        return self.msgNaoEntendi
+
+    elif self.sequencia == 2:
+      if(msgUsuario == '1' or msgUsuario in('NÃO', 'NAO', 'N')):
+        self.sequencia += 1
+        return self.msgTosse
+
+      elif(msgUsuario == '2' or msgUsuario in('ABAIXO DE 38º', 'ABAIXO DE 38')):
+        self.sequencia += 1
+        self.risco += 1
+        return self.msgTosse
+
+      elif(msgUsuario == '3' or msgUsuario in('ACIMA', 'ACIMA DE 38º')):
+        self.sequencia += 1
+        self.risco += 2
+        return self.msgTosse
+
+      else:
+        self.sequencia = 0
+        self.risco = 0
+        return self.msgNaoEntendi
+
+    elif self.sequencia == 3:
+      if(msgUsuario == '1' or msgUsuario in('NÃO', 'NAO', 'N')):
+        self.sequencia += 1
+        return self.msgRespiracao
+
+      elif(msgUsuario == '2' or msgUsuario in('TOSSE SECA')):
+        self.sequencia += 1
+        self.risco += 1
+        return self.msgRespiracao
+
+      elif(msgUsuario == '3' or msgUsuario in('TOSSE COM CATARRO')):
+        self.sequencia += 1
+        self.risco += 2
+        return self.msgRespiracao
+
+      else:
+        self.sequencia = 0
+        self.risco = 0
+        return self.msgNaoEntendi
+
+    elif self.sequencia == 4:
+      if(msgUsuario == '1' or msgUsuario in('NÃO', 'NAO', 'N')):
+        self.sequencia += 1
+
+      elif(msgUsuario == '2'):
+        self.sequencia += 1
+        self.risco += 1
+
+      elif(msgUsuario == '3'):
+        self.sequencia += 1
+        self.risco += 2
+
+      else:
+        self.sequencia = 0
+        self.risco = 0
+        return self.msgNaoEntendi    
+      
+      if(self.risco <= 4):
+        return "Baseado em suas respostas, é provável que esta situação NÃO se enquadre como caso suspeito de doença pelo coronavírus.\nNo entanto, isto não se trata de diagnóstico.\nMantenha as condutas de precaução e prevenção."
+      else:
+        return 'Va urgente para um hospital.'
+    
+    elif msgUsuario in('OBRIGADO', 'TCHAU', 'ADEUS', 'ATÉ MAIS', 'ATÉ LOGO'):
+        self.sequencia = 0
+        self.risco = 0
+        return "Fico muito feliz em ter ajudo você, se cuide e fique bem."
+
     else:
-      return 'Salve Jorge'
-
-  def responderUsuario(self, reposta, chat_id):
-    linkDeEnvio = f'{self.url_base}sendMessage?chat_id={chat_id}&text={reposta}'
-    requests.get(linkDeEnvio)
+      self.sequencia = 0
+      self.risco = 0
+      return self.msgNaoEntendi
+      
+  def respondeUsuario(self, resposta, chat_id):
+    link_requisicao = f'{self.url_base}sendMessage?chat_id={chat_id}&text={resposta}'
+    requests.get(link_requisicao)
 
 bot = DoutorCasaBot()
 bot.Iniciar()
